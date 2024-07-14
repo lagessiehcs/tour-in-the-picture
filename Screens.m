ddf895838f38c2b63cbdd0352a49e2199e2ea0f7
quân <116118843+lagessiehcs@users.noreply.github.com>
2024-07-14 17:20:02 +0200

    spidery mesh finished

--- Screens.m
+++ Screens.m
@@ -1,7 +1,8 @@
 classdef Screens < uint8
     enumeration
-        AddImage         (1) 
-        ChooseForeground (2)
+        AddImage               (1) 
+        ChooseForeground       (2)
+        SeparateWallBackground (3)
     end
 end
 
