9ea8c5293383734c5860d33c97d9d9d428eaf231
quân <116118843+lagessiehcs@users.noreply.github.com>
2024-07-20 11:54:41 +0200

    small modification

--- helper_functions/get3DRoom.m
+++ helper_functions/get3DRoom.m
@@ -1,20 +1,27 @@
-function get3DRoom(floor, left, ceiling, right, rear, twelfPoints, ax)
+function get3DRoom(texture, twelfPoints, ax)
     % This function constructs the 3D room by generating the surface images
     % for all 5 walls
 
-    twelfPoints = twelfPoints';
-    
+    twelfPoints = twelfPoints';    
     
     x = twelfPoints(:, 1);
     y = twelfPoints(:, 2);
     z = twelfPoints(:, 3);
     
-    % figure;
-    surface([x(7), x(8); x(1), x(2)], [y(7), y(8); y(1), y(2)], [z(7), z(8); z(1), z(2)],'FaceColor', 'texturemap', 'CData', rear, 'Parent', ax);
-    surface([x(9), x(10); x(7), x(8)], [y(9), y(10); y(7), y(8)], [z(9), z(10); z(7), z(8)],'FaceColor', 'texturemap', 'CData', ceiling, 'Parent', ax);
-    surface([x(1), x(2); x(3), x(4)], [y(1), y(2); y(3), y(4)], [z(1), z(2); z(3), z(4)],'FaceColor', 'texturemap', 'CData', floor, 'Parent', ax);
-    surface([x(11), x(7); x(5), x(1)], [y(11), y(7); y(5), y(1)], [z(11), z(7); z(5), z(1)],'FaceColor', 'texturemap', 'CData', left, 'Parent', ax);
-    surface([x(8), x(12); x(2), x(6)], [y(8), y(12); y(2), y(6)], [z(8), z(12); z(2), z(6)],'FaceColor', 'texturemap', 'CData', right, 'Parent', ax);
+    surface([x(7), x(8); x(1), x(2)], [y(7), y(8); y(1), y(2)], [z(7), z(8); z(1), z(2)], ...
+        'FaceColor', 'texturemap', 'CData', texture.Rear, 'Parent', ax);
+
+    surface([x(9), x(10); x(7), x(8)], [y(9), y(10); y(7), y(8)], [z(9), z(10); z(7), z(8)], ...
+        'FaceColor', 'texturemap', 'CData', texture.Ceiling, 'Parent', ax);
+
+    surface([x(1), x(2); x(3), x(4)], [y(1), y(2); y(3), y(4)], [z(1), z(2); z(3), z(4)], ...
+        'FaceColor', 'texturemap', 'CData', texture.Floor, 'Parent', ax);
+
+    surface([x(11), x(7); x(5), x(1)], [y(11), y(7); y(5), y(1)], [z(11), z(7); z(5), z(1)], ...
+        'FaceColor', 'texturemap', 'CData', texture.Left, 'Parent', ax);
+
+    surface([x(8), x(12); x(2), x(6)], [y(8), y(12); y(2), y(6)], [z(8), z(12); z(2), z(6)], ...
+        'FaceColor', 'texturemap', 'CData', texture.Right, 'Parent', ax);
     
     axis(ax, 'equal');
     axis(ax, 'vis3d');
@@ -27,3 +34,5 @@ function get3DRoom(floor, left, ceiling, right, rear, twelfPoints, ax)
     rotate3d(ax, "on");
     % campos([0, 0, -100])
 end
+
+
--- helper_functions/getTexture.m
+++ helper_functions/getTexture.m
@@ -1,10 +1,10 @@
-function [floor, left, ceiling, right, rear] = getTexture(Image, twelPoints, out_size)
+function texture = getTexture(Image, twelPoints, out_size)
 
-floor = transformImage(Image, [twelPoints(:, 1)'; twelPoints(:, 2)'; twelPoints(:, 4)'; twelPoints(:, 3)'], out_size);
-left = transformImage(Image, [twelPoints(:, 11)'; twelPoints(:, 7)'; twelPoints(:, 1)'; twelPoints(:, 5)'], out_size);
-ceiling = transformImage(Image, [twelPoints(:, 9)'; twelPoints(:, 10)'; twelPoints(:, 8)'; twelPoints(:, 7)'], out_size);
-right = transformImage(Image, [twelPoints(:, 8)'; twelPoints(:, 12)'; twelPoints(:, 6)'; twelPoints(:, 2)'], out_size);
-rear = transformImage(Image, [twelPoints(:, 7)'; twelPoints(:, 8)'; twelPoints(:, 2)'; twelPoints(:, 1)'], out_size);
+texture.Floor = transformImage(Image, [twelPoints(:, 1)'; twelPoints(:, 2)'; twelPoints(:, 4)'; twelPoints(:, 3)'], out_size);
+texture.Left = transformImage(Image, [twelPoints(:, 11)'; twelPoints(:, 7)'; twelPoints(:, 1)'; twelPoints(:, 5)'], out_size);
+texture.Ceiling = transformImage(Image, [twelPoints(:, 9)'; twelPoints(:, 10)'; twelPoints(:, 8)'; twelPoints(:, 7)'], out_size);
+texture.Right = transformImage(Image, [twelPoints(:, 8)'; twelPoints(:, 12)'; twelPoints(:, 6)'; twelPoints(:, 2)'], out_size);
+texture.Rear = transformImage(Image, [twelPoints(:, 7)'; twelPoints(:, 8)'; twelPoints(:, 2)'; twelPoints(:, 1)'], out_size);
 end
 
 
